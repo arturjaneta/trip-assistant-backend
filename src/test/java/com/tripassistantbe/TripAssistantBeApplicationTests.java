72fa498665959ab5d248318e4767a02c9ad06a64
Pawcio <pawel.golebiowski1998@gmail.com>
2026-04-23 19:50:23 +0200

    init

---
+++ src/test/java/com/tripassistantbe/TripAssistantBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tripassistantbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripAssistantBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
